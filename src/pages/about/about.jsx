2fd7cd58d3f69514cff4ffe024ca61df6c74a2ee
Francis Targanski <francis@targanski.com>
2022-10-11 15:32:57 -0300

    modal

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -10,9 +10,11 @@ import {
   ImageListItem,
   ImageListItemBar,
   Modal,
+  Stack,
 } from "@mui/material";
 import Strava from "../../assets/img/strava-orange.png";
 import { Link } from "react-router-dom";
+import bikefit from "../../assets/img/bikefit.jpg";
 
 const About = () => {
   const [open, setOpen] = React.useState(false);
@@ -88,39 +90,35 @@ const About = () => {
           Bike Fit
         </Typography>
         <div className="boxTotal">
-          <Grid container spacing={2}>
-            <Grid item>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-              voluptatem et laboriosam in deserunt, eius aliquid fugiat,
-              repellat repellendus doloribus vero maxime voluptates odit, quasi
-              corporis porro odio amet unde.
-            </Grid>
-            <Grid item>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed
-              error ipsam ipsum itaque aliquid rem sapiente cupiditate sint
-              doloremque, nisi, quidem provident voluptates consequatur ex iste,
-              nostrum debitis tenetur! Laborum.
-            </Grid>
-            <Grid item>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed
-              error ipsam ipsum itaque aliquid rem sapiente cupiditate sint
-              doloremque, nisi, quidem provident voluptates consequatur ex iste,
-              nostrum debitis tenetur! Laborum.
-            </Grid>
-            <Grid item>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed
-              error ipsam ipsum itaque aliquid rem sapiente cupiditate sint
-              doloremque, nisi, quidem provident voluptates consequatur ex iste,
-              nostrum debitis tenetur! Laborum.
-            </Grid>
-            <Grid item>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed
-              error ipsam ipsum itaque aliquid rem sapiente cupiditate sint
-              doloremque, nisi, quidem provident voluptates consequatur ex iste,
-              nostrum debitis tenetur! Laborum.
-            </Grid>
-          </Grid>
+
+        <Stack spacing={2} className="stackImage">
+          <img src={bikefit} alt="Logo" />
+        </Stack>
+
+          <Typography
+            spacing={2}
+            variant="p"
+            component="p"
+            sx={{ paddingTop: "16px" }}
+          >
+            Bike Fit é o processo de ajustar uma bicicleta para um ciclista para
+            otimizar seu conforto, desempenho e eficiência. Os ciclistas
+            geralmente sofrem lesões por uso excessivo, como paralisia do
+            ciclista, costas do ciclista e dor anterior no joelho . e isso
+            geralmente se deve a um ciclo configurado incorretamente.
+          </Typography>
+          <Typography variant="p" component="p" sx={{ paddingTop: "16px" }}>
+            O bike fit visa prevenir lesões, aumentar a eficiência, conforto e
+            melhorar o desempenho de todos os ciclistas. O ajuste da bicicleta é
+            para qualquer pessoa que anda de bicicleta, principalmente se
+            ocorreu uma lesão por uso excessivo, para maximizar sua diversão e
+            desempenho.
+          </Typography>
+
+
+
         </div>
+
       </Grid>
 
       <Grid item xs={12} md={12} lg={12} sx={{ padding: "20px !important" }}>
@@ -170,47 +168,10 @@ const About = () => {
             onClose={handleClose}
             aria-labelledby="modal-modal-title"
           >
-            <Grid container spacing={5} className="modal">
-              <Grid item xs={12} md={12} lg={12}>
-                <Typography id="modal-modal-title" variant="h4" component="h3">
-                  {atletaList?.atleta}
-                </Typography>
-
-                <Typography variant="h5" component="h3">
-                  Equipe: {atletaList?.equipe}
-                </Typography>
-
-                <Grid container>
-                  <Grid item>
-                    <Typography variant="h6" component="h4">
-                      Nacionalidade: {atletaList?.pais}
-                    </Typography>
-                  </Grid>
-                  <Grid item>
-                    <div className="flagSpan">
-                      <img
-                        className="flag"
-                        src={`${atletaList?.flag}`}
-                        srcSet={`${atletaList?.flag}`}
-                        alt={atletaList.flag}
-                        loading="lazy"
-                      />
-                    </div>
-                  </Grid>
-                </Grid>
-
-                <Typography variant="p" component="p">
-                  Vitorias: {atletaList?.vitorias}
-                </Typography>
-
-                <Typography variant="p" component="p">
-                  Podiums: {atletaList?.podiums}
-                </Typography>
-              </Grid>
-
-              <Grid item xs={12} md={12} lg={12}>
+            <Grid container spacing={2} className="modal">
+              <Grid item xs={12} md={5} lg={5} className="imageModalBox">
                 <img
-                  style={{ filter: "drop-shadow(4px 4px 6px #222)" }}
+                  className="imageModal"
                   src={`${atletaList?.img}`}
                   srcSet={`${atletaList?.img}`}
                   alt={atletaList.atleta}
@@ -218,7 +179,44 @@ const About = () => {
                 />
               </Grid>
 
-              <Grid item xs={12} md={12} lg={12} sx={{paddingBottom:"15px", justifyItems: "center"}}>
+              <Grid item xs={12} md={7} lg={7} className="modalText">
+                <Typography
+                  fullwidth
+                  id="modal-modal-title"
+                  variant="h4"
+                  component="h4"
+                >
+                  {atletaList?.atleta}
+                </Typography>
+
+                <Typography variant="h6" component="h6">
+                  Equipe: {atletaList?.equipe}
+                </Typography>
+
+                <Grid container className="country">
+                  <Typography variant="p" component="p">
+                    Nacionalidade: {atletaList?.pais}
+                  </Typography>
+
+                  <div className="flagSpan">
+                    <img
+                      className="flag"
+                      src={`${atletaList?.flag}`}
+                      srcSet={`${atletaList?.flag}`}
+                      alt={atletaList.flag}
+                      loading="lazy"
+                    />
+                  </div>
+                </Grid>
+
+                <Typography variant="p" component="p">
+                  Vitórias: {atletaList?.vitorias}
+                </Typography>
+
+                <Typography variant="p" component="p">
+                  Podiums: {atletaList?.podiums}
+                </Typography>
+
                 <Typography variant="h6" component="h6">
                   Perfil Strava:{" "}
                   <IconButton edge="end" className="button">
